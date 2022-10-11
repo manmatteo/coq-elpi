db69b5564108526e929450002164a273a8a27a8e
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-10-11 16:53:13 +0200

    abstract universes from clauses

--- src/coq_elpi_builtins.ml
+++ src/coq_elpi_builtins.ml
@@ -118,7 +118,7 @@ let on_global_state_does_rewind_env api thunk = (); (fun state ->
   let state, result, gls = thunk state in
   Coq_elpi_HOAS.grab_global_env_drop_univs state, result, gls)
 
-let warn_if_contains_univ_levels ~depth t =
+let err_if_contains_alg_univ ~depth t =
   let global_univs = UGraph.domain (Environ.universes (Global.env ())) in
   let is_global u = 
     match Univ.Universe.level u with
@@ -126,13 +126,20 @@ let warn_if_contains_univ_levels ~depth t =
     | Some l -> Univ.Level.Set.mem l global_univs in
   let rec aux ~depth acc t =
     match E.look ~depth t with
-    | E.CData c when isuniv c -> let u = univout c in if is_global u then acc else u :: acc
+    | E.CData c when isuniv c ->
+        let u = univout c in
+        if is_global u then acc
+        else
+          begin match Univ.Universe.level u with
+          | None ->
+            err Pp.(strbrk "The hypothetical clause contains terms of type univ which are not global, you should abstract them out or replace them by global ones: " ++
+                      Univ.Universe.pr u)
+          | _ -> Univ.Universe.Set.add u acc
+          end
     | x -> Coq_elpi_utils.fold_elpi_term aux acc ~depth x
   in
-  let univs = aux ~depth [] t in
-  if univs <> [] then
-    err Pp.(strbrk "The hypothetical clause contains terms of type univ which are not global, you should abstract them out or replace them by global ones: " ++
-            prlist_with_sep spc Univ.Universe.pr univs)
+  let univs = aux ~depth Univ.Universe.Set.empty t in
+  univs
 ;;
 
 let bool = B.bool
@@ -3536,7 +3543,36 @@ Supported attributes:
   (fun scope dbname (name,graft,clause) ~depth ctx _ state ->
      let loc = API.Ast.Loc.initial "(elpi.add_clause)" in
      let dbname = Coq_elpi_utils.string_split_on_char '.' dbname in
-     warn_if_contains_univ_levels ~depth clause;
+     let levels_to_abstract = err_if_contains_alg_univ ~depth clause in
+     let levels_to_abstract_no = Univ.Universe.Set.cardinal levels_to_abstract in
+     let rec subst ~depth m t =
+       match E.look ~depth t with
+       | E.CData c when isuniv c ->
+           begin try E.mkBound (Univ.Universe.Map.find (univout c) m)
+           with Not_found -> t end
+       | E.App(c,x,xs) ->
+           E.mkApp c (subst ~depth m x) (List.map (subst ~depth m) xs)
+       | E.Cons(x,xs) ->
+           E.mkCons (subst ~depth m x) (subst ~depth m xs)
+       | E.Lam x ->
+           E.mkLam (subst ~depth:(depth+1) m x)
+       | E.Builtin(c,xs) ->
+           E.mkBuiltin c (List.map (subst ~depth m) xs)
+       | E.UnifVar _ -> assert false
+       | E.Const _ | E.Nil | E.CData _ -> t
+       in
+     let clause = 
+       let rec bind d map = function
+        | [] ->
+            subst ~depth:d map
+              (API.Utils.move ~from:depth ~to_:(depth + levels_to_abstract_no) clause)
+        | l :: ls ->
+          E.mkApp E.Constants.pic (E.mkLam (*   pi x\  *)
+              (bind (d+1) (Univ.Universe.Map.add l d map) ls)) []
+        in
+          bind depth Univ.Universe.Map.empty
+            (Univ.Universe.Set.elements levels_to_abstract)
+     in
      let vars = collect_term_variables ~depth clause in
      let clause = U.clause_of_term ?name ?graft ~depth loc clause in
      let local = ctx.options.local = Some true in
